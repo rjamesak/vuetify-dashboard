70f17e67bc217926fcf110a8d0f185ca9eec6c19
Russell James <jamesru@oregonstate.edu>
2020-12-24 12:51:34 -0900

    add router, dashboard view with data table, employee data

--- src/App.vue
+++ src/App.vue
@@ -8,48 +8,17 @@
       <v-toolbar-title>Vuetify Dashboard
       </v-toolbar-title>
       <v-spacer></v-spacer>
-        <v-btn text rounded>Home</v-btn>
-        <v-btn text rounded>Login</v-btn>
+        <v-btn 
+          v-for="link in links"
+          :key="`${link.label}-header-link`"
+          text 
+          rounded 
+          :to="link.url">
+          {{link.label}}
+          </v-btn>
     </v-app-bar>
     <v-main>
-
-    <!-- Login Module -->
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field 
-          label="Username"
-          prepend-icon="mdi-account-circle"
-          ></v-text-field>
-          <v-text-field 
-          :type="showPassword ? 'text' : 'password'" 
-          label="Password"
-          prepend-icon="mdi-lock"
-          :append-icon= "showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="showPassword = !showPassword"
-          ></v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn
-        color="success"
-        >Register
-        </v-btn>
-        <v-spacer></v-spacer>
-
-        <v-btn
-        color="info"
-        >Login
-        </v-btn>
-
-      </v-card-actions>
-    </v-card>
-    <!-- End Login Module -->
-
+      <router-view></router-view>
     </v-main>
 
     <!-- footer -->
@@ -63,13 +32,14 @@
       >
         <v-btn
           v-for="link in links"
-          :key="link"
+          :to="link.url"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col
           class="primary lighten-2 py-4 text-center white--text"
@@ -93,12 +63,20 @@ export default {
 
 data() {
   return {
-    showPassword: false,
     links: [
-      'Home',
-      'Login',
-      'About',
-      'Contact'
+      {
+        label: 'Home', 
+        url: '/'
+      },
+      {
+        label: 'Login', 
+        url: '/login'
+      },
+      {
+        label: 'Dashboard',
+        url: '/dashboard'
+      }
+      
     ]
   }
 },
